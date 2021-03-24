a6c5d0d61d49ecefebc7257b60bcd2632761e09f
Jess on the Hope <hon898@gmail.com>
2021-03-23 21:40:26 -0400

    hooked up binding on fragment edit.

--- fix_it/app/src/main/java/com/bitpunchlab/fix_it/EditFragment.kt
+++ fix_it/app/src/main/java/com/bitpunchlab/fix_it/EditFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.bitpunchlab.fix_it.databinding.FragmentEditBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -18,7 +20,10 @@ class EditFragment : Fragment() {
 			savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_edit, container, false)
+		val binding: FragmentEditBinding = DataBindingUtil.inflate(inflater,
+			R.layout.fragment_edit, container, false)
+		binding.lifecycleOwner = this
+		return binding.root
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
